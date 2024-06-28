051ccbe65bbb025acd91de4457701cd272fca91e
silasms <silas.medeiros7@gmail.com>
2024-06-27 22:55:04 -0300

    refactor: set jwt with token module

--- src/service/token/token.module.ts
+++ src/service/token/token.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TokenService } from './token.service';
+import { ConfigModule } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   providers: [TokenService],
-  exports: [TokenService]
+  exports: [TokenService],
+  imports: [ConfigModule.forRoot(), JwtModule]
 })
 export class TokenModule {}
--- src/service/token/token.service.ts
+++ src/service/token/token.service.ts
@@ -1,26 +1,33 @@
-import { Injectable, Logger, UnauthorizedException } from '@nestjs/common';
-import { hash, verify } from 'argon2';
+import { Injectable, Logger } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
-export class TokenService {
-  async createToken(password: string) {
-    try {
-      const token = await hash(password);
-      return token
-    } catch(err) {
-      Logger.error(err)
-    }
+export class TokenService<T extends string | object | Buffer = any> {
+  private readonly logger = new Logger(TokenService.name);
+  constructor(private jwtService: JwtService) {}
+
+  async createToken(payload: T, neverExpires?: boolean) {
+    const options: {
+      secret: string;
+      expiresIn?: string;
+    } = {
+      secret: process.env.SECRET,
+    };
+    if (!neverExpires) options.expiresIn = '12h';
+
+    return this.jwtService.signAsync(payload as object, options);
   }
 
-  async verifyToken(token: string, password: string) {
-    try {
-      if (await verify(token, password)) {
-        return token
-      } else {
-        throw new UnauthorizedException('Password is invalid')
-      }
-    } catch(err) {
-      Logger.error(err)
-    }
+  async decodeToken(token: string) {
+    return (await this.jwtService
+      .verifyAsync(token, { secret: process.env.SECRET })
+      .catch((err) => {
+        this.logger.error(err.message, err);
+        null;
+      })) as T;
+  }
+
+  async decodeUser(token: string) {
+    return (await this.jwtService.decode(token)) as T;
   }
 }
