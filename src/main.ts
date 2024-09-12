4196c3def4b260ebbe39f11ad7eb6edbe20fe324
silasms <silas.medeiros7@gmail.com>
2024-09-12 06:54:54 -0300

    feat: increase requests limit

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { json, urlencoded } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors()
   app.useGlobalPipes(new ValidationPipe())
+  
+  app.use(json({ limit: '50mb' }));
+  app.use(urlencoded({ extended: true, limit: '50mb' }));
   await app.listen(3000);
 }
 bootstrap();
